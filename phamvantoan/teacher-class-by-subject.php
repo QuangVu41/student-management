a2005b98c6b5f1e2ca0afa57d2f2467c344e827c
phamvantoan <phamvantoan231003@gmail.com>
2024-01-01 21:11:40 +0700

    lan7

--- phamvantoan/teacher-class-by-subject.php
+++ phamvantoan/teacher-class-by-subject.php
@@ -14,7 +14,7 @@
     if(isset($_SESSION['teacher_id'])){
         $classBySubject = classBySubject($_SESSION['teacher_id']);
 ?>
-        <div class="container">
+        <div class="container" style="margin-bottom: 500px">
 <?php   for($i = 0; $i < $classBySubject->num_rows; $i++){
             $row = $classBySubject->fetch_assoc();
             $className = $row["class_name"];
@@ -38,6 +38,7 @@
         <p>Vui lòng đăng nhập trước!</p>
 <?php
     }
+    require 'teacher-footer.php';
 ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
